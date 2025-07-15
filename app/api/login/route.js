353dcc34b2a557eda69510bba762c0de6584c772
Harsh <jatoliyaharsh8@gmail.com>
2025-07-15 10:51:07 +0530

    updated authenticatiion

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import connectDB from '@/lib/db';
+import connectDB from '@/lib/mongodb';
 import User from '@/models/User';
 
 export async function POST(req) {
